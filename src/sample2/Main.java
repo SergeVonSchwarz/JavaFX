e23bbfa366636d0d1db5d84de18b35dcea34844b
tolstopus <sergio.glock@gmail.com>
2019-02-07 18:50:57 +0200

    icon

--- src/sample2/Main.java
+++ src/sample2/Main.java
@@ -1,10 +1,13 @@
 package sample2;
 
 import javafx.application.Application;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.Group;
 import javafx.scene.text.Text;
+
+import static com.sun.org.apache.bcel.internal.util.SecuritySupport.getResourceAsStream;
 
 public class Main extends Application{
 
@@ -25,6 +28,7 @@ public class Main extends Application{
 
         Scene scene = new Scene(group);
         stage.setScene(scene);
+        stage.getIcons().add(new Image(Main.class.getResourceAsStream("ico.png")));
         stage.setTitle("First Application");
         stage.setWidth(300);
         stage.setHeight(250);

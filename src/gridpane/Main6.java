02b8fd176c148c1788507a09faadaa2a33175e1a
tolstopus <sergio.glock@gmail.com>
2019-02-16 14:40:46 +0200

    gridpane

---
+++ src/gridpane/Main6.java
@@ -0,0 +1,58 @@
+package gridpane;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.RowConstraints;
+
+public class Main6 extends Application{
+
+    public static void main(String[] args) {
+
+        Application.launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) {
+
+        Button first = new Button("First");
+        Button second = new Button("Second");
+        Button third = new Button("Third");
+        Button fourth = new Button("Fourth");
+
+        GridPane root = new GridPane();
+
+        ColumnConstraints column1 = new ColumnConstraints();
+        column1.setPercentWidth(50);
+        root.getColumnConstraints().add(column1);
+
+        ColumnConstraints column2 = new ColumnConstraints();
+        column2.setPercentWidth(50);
+        root.getColumnConstraints().add(column2);
+
+        RowConstraints row1 = new RowConstraints();
+        row1.setPercentHeight(50);
+        root.getRowConstraints().add(row1);
+
+        RowConstraints row2 = new RowConstraints();
+        row2.setPercentHeight(50);
+        root.getRowConstraints().add(row2);
+
+        root.setGridLinesVisible(true);
+        root.add(first, 0, 0);
+        root.add(second, 0, 1);
+        root.add(third, 1, 0);
+        root.add(fourth, 1, 1);
+
+
+        Scene scene = new Scene(root, 300, 200);
+        stage.setScene(scene);
+
+        stage.setTitle("GridPane in JavaFX");
+
+        stage.show();
+    }
+}

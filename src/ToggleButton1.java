ec6010dfa7844919dc6e32299f5a58e5eb822ea0
tolstopus <sergio.glock@gmail.com>
2019-02-19 09:20:15 +0200

    toggle button

---
+++ src/ToggleButton1.java
@@ -0,0 +1,42 @@
+import javafx.application.Application;
+import javafx.scene.control.ToggleButton;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.control.Label;
+import javafx.geometry.Insets;
+
+public class ToggleButton1 extends Application {
+    public static void main(String[] args) {
+
+        Application.launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        ToggleButton lightBtn = new ToggleButton("Light");
+        Label stateLbl = new Label();
+        // обработка нажатия
+        lightBtn.setOnAction(event -> {
+
+            if(lightBtn.isSelected()){
+                stateLbl.setText("Light on!");
+            }
+            else{
+                stateLbl.setText("Light off...");
+            }
+        });
+
+        FlowPane root = new FlowPane(10, 10);
+        root.getChildren().addAll(lightBtn, stateLbl);
+        root.setPadding(new Insets(10));
+        Scene scene = new Scene(root, 250, 200);
+
+        stage.setScene(scene);
+
+        stage.setTitle("Hello JavaFX");
+
+        stage.show();
+    }
+}
